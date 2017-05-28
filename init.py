55b474f8c4ffa0afba7af51408172eb49a8c106b
neeraj <neeraj11thekid@gmail.com>
2017-05-28 22:46:50 +0530

    major changes, script runs automatically when wifi interface's status changes

--- init.py
+++ init.py
@@ -2,21 +2,31 @@ import json
 import os
 import subprocess
 
-username = input("Enter your username : ").strip()
-password = input("Enter your password : ").strip()
+username = raw_input("Enter your username : ").strip()
+password = raw_input("Enter your password : ").strip()
 
-fileobj = open("information.json","w")
+fileobj = open("login","w")
 info = {}
 info["username"] = username
 info["password"] = password
-json.dump(info,fileobj)
+temp = "#!/usr/bin/env python\n\ninfo = " + str(info) + "\n\n"
+fileobj.write(temp)
 fileobj.close()
-subprocess.Popen("cp auth information.json /etc/network/if-up.d/;chmod +x /etc/network/if-up.d/auth", shell=True)
+
+fileobj = open("login","a")
+fileobj2 = open("auth","r")
+temp = fileobj2.read()
+fileobj.write(temp)
+fileobj.close()
+fileobj2.close()
+
+subprocess.Popen("cp login /etc/network/if-up.d/;chmod +x /etc/network/if-up.d/login", shell=True)
+subprocess.Popen("rm login", shell=True)
 outp = subprocess.Popen("ls /sys/class/net/", stdout=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
 out,err = outp.communicate()
 out = out.decode("utf-8").split()
 out.sort()
-string = "\nauto " + out[-1] + "\npost-up /etc/network/if-up.d/auth"
+string = "\nauto " + out[-1]
 fileobj = open("/etc/network/interfaces","a")
 fileobj.write(string)
 fileobj.close()
